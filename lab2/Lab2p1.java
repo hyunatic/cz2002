deb3fa20c1bff925942b0eac6dd46f1bbf0326e6
Iskandar <habbo_armageddon@hotmail.com>
2020-08-19 20:56:56 +0800

    Lab 2 almost done

---
+++ lab2/Lab2p1.java
@@ -0,0 +1,128 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lab2p1 {
+
+    public static void main(String[] args) {
+        int choice;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Perform the following methods:");
+            System.out.println("1: miltiplication test");
+            System.out.println("2: quotient using division by subtraction");
+            System.out.println("3: remainder using division by subtraction");
+            System.out.println("4: count the number of digits");
+            System.out.println("5: position of a digit");
+            System.out.println("6: extract all odd digits");
+            System.out.println("7: quit");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    mulTest();
+                    break;
+                case 2: {
+                    System.out.print("Enter Quotient Number:");
+                    int m = sc.nextInt();
+                    System.out.print("Enter Divisor:");
+                    int n = sc.nextInt();
+                    int result = divide(m, n);
+                    System.out.println(m + "/" + n + "=" + result);
+                    break;
+                }
+                case 3: {
+                    System.out.print("Enter Quotient Number:");
+                    int m1 = sc.nextInt();
+                    System.out.print("Enter Divisor:");
+                    int n1 = sc.nextInt();
+                    int result1 = modulus(m1, n1);
+                    System.out.println(m1 + "%" + n1 + "=" + result1);
+                    break;
+                }
+                case 4: {
+                    System.out.print("Enter number:");
+                    int number = sc.nextInt();
+                    int result2 = countDigits(number);
+                    if (result2 == 0)
+                        System.out.println("Error input!!");
+                    else
+                        System.out.println("n : " + number + " - count = " + result2);
+                }
+                    break;
+                case 5:
+                    System.out.print("Enter number:");
+                    int number = sc.nextInt();
+                    System.out.println("Enter Digit:");
+                    int digit = sc.nextInt();
+                    int result3 = position(number,digit);
+                    System.out.println("position = " + result3);
+                    break;
+                case 6: /* add extractOddDigits() call */
+                    break;
+                case 7:
+                    System.out.println("Program terminating ….");
+            }
+        } while (choice < 7);
+    }
+
+    public static void mulTest() {
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+        int correct = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int ran_int1 = rand.nextInt(10) + 1;
+            int ran_int2 = rand.nextInt(10) + 1;
+
+            int result = ran_int1 * ran_int2;
+            System.out.println("How much is " + ran_int1 + " times " + ran_int2);
+            int input = sc.nextInt();
+
+            if (result == input)
+                correct++;
+        }
+        System.out.println(correct + " answer out of 5 are correct");
+    }
+
+    public static int divide(int m, int n) {
+        if (m < n)
+            return 0;
+        int result = 0;
+        do {
+            m -= n;
+            result++;
+        } while (m > 0);
+        return result--;
+    }
+
+    public static int modulus(int m, int n) {
+        do {
+            m -= n;
+        } while (m > 0);
+        System.out.println(m);
+        return (m < 0) ? m + n : 0;
+    }
+
+    public static int countDigits(int n) {
+        if (n < 0) {
+            return 0;
+        }
+        int result = 0;
+        while (n > 0) {
+            n /= 10;
+            result++;
+        }
+        return result--;
+    }
+
+    public static int position(int n, int digit) {
+        int totaldigit = countDigits(n);
+        int result = 0;
+        for (int i = 0; i < totaldigit; i++) {
+            if (n % 10 == digit)
+                result = i;
+            n /= 10;
+        }
+        
+        return (result == 0) ? -1 : ++result;
+    }
+}
